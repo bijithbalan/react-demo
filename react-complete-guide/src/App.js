accbebb112c1b8cf875f99f5e79e422a00e02c77
Bijith Balan <bijithbalan@gmail.com>
2020-01-04 19:15:41 -0800

    Style Sheets

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -7,26 +7,45 @@ class App extends Component {
   state = {
     people: [
       { name: 'John', age: 28 },
-      { name: 'Rex', age: 27 }
+      { name: 'Alice', age: 25 },
+      { name: 'Rex', age: 27 },
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     this.setState({
       people: [
-        { name: 'Johnson', age: 28 },
+        { name: newName, age: 28 },
+        { name: 'Alice', age: 25 },
+        { name: 'Rex', age: 27 }
+      ]
+    })
+  }
+
+  switchAgeHandler = (newAge) => {
+    this.setState({
+      people: [
+        { name: 'John', age: 28 },
+        { name: 'Alice', age: newAge },
         { name: 'Rex', age: 27 }
       ]
     })
   }
 
   render() {
+    const styles = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px'
+    }
     return (
       <div className="App">
-        <h1>I am a React App</h1>
+        <h1 style={styles} >I am a React App</h1>
         <Person name={this.state.people[0].name} age={this.state.people[0].age} />
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.people[1].name} age={this.state.people[1].age}>My hobby is stamp collection.</Person>
+        <button onClick={this.switchNameHandler.bind(this, 'Johnson')}>Switch John's Name</button>
+        <Person name={this.state.people[1].name} age={this.state.people[1].age} click={() => this.switchAgeHandler(26)} />
+        <Person name={this.state.people[2].name} age={this.state.people[2].age}>My hobby is stamp collection.</Person>
       </div>
     );
     // only changes in props and/ or state can trigger React to re-render the components 
@@ -34,6 +53,7 @@ class App extends Component {
     // return React.createElement('div', {className: 'App' }, React.createElement('h1', null, 'I am a React App'));
     // min 3 elements, max infinite elements
     // null is for configuration
+    // .bind() is more efficient
   }
 }
 
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     // props is managed from outside the component
     return (
-        <div>
-            <p>I am {props.name} and I am {props.age} year old! {props.children}</p>
-        </div>
+        <div className="Person">
+            <p onClick={props.click}>I am {props.name} and I am {props.age} year old! {props.children}</p>
+        </div >
     )
 }
 // when using class-based components, it's this.props
